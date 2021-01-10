e583de24b4a01736930451b3e1cdbc6dddff0611
Demmy Honore de Vries <demmyhonore@gmail.com>
2021-01-10 14:06:12 +0100

    Add font Noto Sans JP to project

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import {
+  useFonts,
+  NotoSansJP_500Medium,
+} from "@expo-google-fonts/noto-sans-jp";
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    NotoSansJP_500Medium,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{ fontFamily: "NotoSansJP_500Medium", fontSize: 200 }}>
+        お
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +28,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
