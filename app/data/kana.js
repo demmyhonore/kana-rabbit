dd40e353525392c772d707dbe7d28d8ccafe79f0
Demmy Honore de Vries <demmyhonore@gmail.com>
2021-01-18 22:58:31 +0100

    Add first 10 hiragana as data

---
+++ app/data/kana.js
@@ -0,0 +1,52 @@
+export default [
+  {
+    letter: "あ",
+    pronunciation: "a",
+    group: "hiragana",
+  },
+  {
+    letter: "か",
+    pronunciation: "ka",
+    group: "hiragana",
+  },
+  {
+    letter: "さ",
+    pronunciation: "sa",
+    group: "hiragana",
+  },
+  {
+    letter: "た",
+    pronunciation: "ta",
+    group: "hiragana",
+  },
+  {
+    letter: "な",
+    pronunciation: "na",
+    group: "hiragana",
+  },
+  {
+    letter: "い",
+    pronunciation: "i",
+    group: "hiragana",
+  },
+  {
+    letter: "き",
+    pronunciation: "ki",
+    group: "hiragana",
+  },
+  {
+    letter: "し",
+    pronunciation: "shi",
+    group: "hiragana",
+  },
+  {
+    letter: "ち",
+    pronunciation: "chi",
+    group: "hiragana",
+  },
+  {
+    letter: "に",
+    pronunciation: "ni",
+    group: "hiragana",
+  },
+];
