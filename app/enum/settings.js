436ecc5e85031aec1bbfdc552cce8fda8ee6ad64
Demmy Honore de Vries <demmyhonore@gmail.com>
2021-02-07 11:30:52 +0100

    Add enum for settings

---
+++ app/enum/settings.js
@@ -0,0 +1,18 @@
+const actionTypes = {
+  SELECT_KANA: "selectKana",
+  SELECT_LEARNING_MODE: "selectLearningMode",
+};
+
+const kanaType = {
+  HIRAGANA: "hiragana",
+  KATAKANA: "katakana",
+  DIACRITICAL_MARKS: "diacriticalMarks",
+  CONTRACTED: "contracted",
+};
+
+const learningMode = {
+  RANDOM_ORDER: "randomOrder",
+  NEWBIE_WAY: "newbieWay",
+};
+
+export { actionTypes, kanaType, learningMode };
