4ed8be1ad0bb98850bbc52db35adc1259d5aaec9
Demmy Honore de Vries <demmyhonore@gmail.com>
2021-02-12 14:08:34 +0100

    Add fonts lemon and lexend mega to project

--- App.js
+++ App.js
@@ -1,17 +1,19 @@
 import React from "react";
-
 import AppLoading from "expo-app-loading";
-import {
-  useFonts,
-  KosugiMaru_400Regular,
-} from "@expo-google-fonts/kosugi-maru";
+import { useFonts, Lemon_400Regular } from "@expo-google-fonts/lemon";
+import { LexendMega_400Regular } from "@expo-google-fonts/lexend-mega";
+import { KosugiMaru_400Regular } from "@expo-google-fonts/kosugi-maru";
 
 import { SettingsProvider } from "./app/context/settings";
 import { KanaProvider } from "./app/context/kana";
 import KanaScreen from "./app/screens/kana-screen";
 
 export default function App() {
-  const [fontsLoaded] = useFonts({ KosugiMaru_400Regular });
+  const [fontsLoaded] = useFonts({
+    Lemon_400Regular,
+    LexendMega_400Regular,
+    KosugiMaru_400Regular,
+  });
 
   if (!fontsLoaded) return <AppLoading />;
 
